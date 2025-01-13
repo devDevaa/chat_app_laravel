79abd1aee813fee8c387774134a8b08ba21f5611
Deva <user@Deva07.local>
2025-01-13 23:39:28 +0630

    migration, relation

---
+++ app/Models/Group.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    protected $fillable = ["name", "description", "owner_id", "last_message _id"];
+
+    public function users() {
+        return $this->belongsToMany(User::class, "group_users");
+    }
+
+    public function owner() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function messages() {
+        return $this->hasMany(Message::class);
+    }
+}
---
+++ app/Models/Message.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\MessageAttachment;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $fillable = [
+        'message',
+        'sender_id',
+        'receiver_id',
+        'group_id',
+        'conversation_id'
+    ];
+
+    public function sender() {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver() {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
+    public function group() {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function attachments() {
+        return $this->hasMany(MessageAttachment::class);
+    }
+}
